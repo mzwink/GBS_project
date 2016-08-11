f5de3e4abd130c4b9d78724689c1db628917dcbc
Madison Zwink <mzwink@pugetsound.edu>
2016-08-11 14:52:12 -0400

    Added rollapply function

--- R_scripts/read_depth.R
+++ R_scripts/read_depth.R
@@ -1,9 +1,17 @@
 # Load the data and plot the coverage
 # Path to the BEDGRAPH file
 
+library(zoo)
+library(ggplot)
+
 path = "/Users/madisonzwink/Desktop/GBS_project/demultiplex"
 
 # Change the file_name once coverage has been calculated
 bedgraph_file <- read.delim(file_name, header = FALSE, col.names=c("chromosome", "start", "end", "read_depth"))
 
-rows <- nrow(bedgraph_file)
+# rollapply function to make sliding window
+# play around with values to make best sliding window for data
+
+window_size = 100
+
+loci <- rollapply(bedgraph_file, by = window_size, FUN = mean)
