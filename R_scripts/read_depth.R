9f9c9cdd1b30a16e818f7dcef8394f765bdbedba
Madison Zwink <mzwink@pugetsound.edu>
2016-08-11 11:16:43 -0400

    Added R_scripts directory to repository

---
+++ R_scripts/read_depth.R
@@ -0,0 +1,9 @@
+# Load the data and plot the coverage
+# Path to the BEDGRAPH file
+
+path = "/Users/madisonzwink/Desktop/GBS_project/demultiplex"
+
+# Change the file_name once coverage has been calculated
+bedgraph_file <- read.delim(file_name, header = FALSE, col.names=c("chromosome", "start", "end", "read_depth"))
+
+rows <- nrow(file)
